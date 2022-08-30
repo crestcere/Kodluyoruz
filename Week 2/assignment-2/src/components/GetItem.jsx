f5530b95886b71351acc90e20dee993a382e40bd
iCarly <crestcere@outlook.com>
2022-08-30 17:44:43 +0300

    Sass

--- Week 2/assignment-2/src/components/GetItem.jsx
+++ Week 2/assignment-2/src/components/GetItem.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const GetItem = ({ item }) => {
     return (
-        <div>
-            <div className="GetItem">
-                <p>Name: {item.name}</p>
-                <p>Height: {item.height}</p>
-                <p>Gender: {item.gender}</p>
-                <p>Films: {item.films}</p>
-            </div>
+        <div className="GetItem">
+            <p>Name: {item.name}</p>
+            <p>Height: {item.height}</p>
+            <p>Gender: {item.gender}</p>
+            <p>Films: {item.films}</p>
         </div>
     );
 }
